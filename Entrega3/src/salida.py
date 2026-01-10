2edbde8e8492594206c7e660aa43fb0765f779a7
David Gutiérrez Marjalizo <76493138+TheSweetPancake@users.noreply.github.com>
2026-01-10 12:12:10 +0100

    Update salida.py

--- Entrega3/src/salida.py
+++ Entrega3/src/salida.py
@@ -21,9 +21,6 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 templates_dir = os.path.join(os.path.dirname(script_dir), "templates")
 generado_dir = os.path.join(os.path.dirname(script_dir), "generado")
 
-# Asegurar que el directorio de salida existe
-if not os.path.exists(generado_dir):
-    os.makedirs(generado_dir)
 
 env = Environment(loader=FileSystemLoader(templates_dir))
 template = env.get_template(plantilla)
@@ -37,11 +34,10 @@ html = template.render(
     portafolio=folio.to_dict()
 )
 
-# Cambiamos el nombre del archivo de salida para identificarlo fácilmente
-output_filename = f"cv_generado_persona{PERSONA}.html"
-output_file = os.path.join(generado_dir, output_filename)
 
+output_file = os.path.join(generado_dir, "index.html")
 with open(output_file, "w", encoding="utf-8") as f:
     f.write(html)
 
-print(f"Guardado {output_filename} usando la plantilla: {plantilla}")
+
+print("Guardado index.html")
